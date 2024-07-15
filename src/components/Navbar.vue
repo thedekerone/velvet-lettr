2039bacf6b00e7c7ca09ab4c07c7b73e7da9c2f4
Mauricio Fow <mauricio.fow@quanturi.com>
2024-07-15 09:12:20 +0300

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,11 @@
+<template>
+  <nav class="w-full bg-red-200 flex items-center justify-between py-2 px-8">
+    <h1 class="text-2xl font-bold text-center py-2">VelvetLettr</h1>
+    <ul class="ml-auto w-full max-w-xs flex justify-between">
+      <li><router-link class="text-black hover:text-red-500 hover:cursor-pointer" to="/">Home</router-link></li>
+      <li><router-link class="text-black hover:text-red-500 hover:cursor-pointer" to="/about">About</router-link></li>
+      <li><router-link class="text-black hover:text-red-500 hover:cursor-pointer" to="/contact">Contact</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
